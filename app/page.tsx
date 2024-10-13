83a8ae29b8dc59812848c313330883ead2770be1
Cameron Scott <casctt@clemson.edu>
2024-10-12 22:27:05 -0400

    update: page history

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-center items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-  <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-    <Image
-      className="rounded-full"
-      src="/profile-pic2.jpg" 
-      alt="Custom Image"
-      width={180}
-      height={180}  
-      priority
-    />
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="rounded-full"
+          src="/profile-pic2.jpg" 
+          alt="Custom Image"
+          width={180}
+          height={180}  
+          priority
+        />
         <ol className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -22,22 +23,12 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/about"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            About
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/experience"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Experience
-          </a>
+          <Link href="/about" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+              About
+          </Link>
+          <Link href="/experience"className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+              Experience
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-left justify-center">
